d3bd552cf981e304ab05ab6f3dd0299f481933bb
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-27 10:24:40 -0500

    Add project files.

---
+++ HWDoWhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool exitProgram = false;
+
+do
+{
+    Console.WriteLine("Please enter your name");
+    string name = Console.ReadLine();
+    if (name.ToLower() == "tim")
+    {
+        Console.WriteLine("Welcome Professor Tim");
+    }
+    else
+    {
+        Console.WriteLine($"Welcome to the class {name}");
+    }
+    Console.WriteLine("Press E if you would like to exit the program, else press enter to continue");
+    string exitResponse = Console.ReadLine();
+    if (exitResponse.ToLower() == "e")
+    {
+        exitProgram = true;
+    }
+
+} while (!exitProgram);
